5c843639232e426bb7202363ea3e934cd6af1ac4
Khaan25 <ziarzai@hotmail.com>
2022-05-07 12:24:45 +0500

    Added Files...

--- meta.ts
+++ meta.ts
@@ -3,12 +3,10 @@ import { ResumeriseMeta } from "./core-library.ts";
 export const meta = {
   colors: ["#000", "#fff"],
   description: "A starter template for resumerise.",
-  name: "resumerise_theme_retro",
   platforms: ["DESKTOP", "INTERACTIVE", "MOBILE", "PRINT"],
   tags: ["starter", "blueprint", "retro"],
   categories: ["EDUCATION"],
   styles: ["CLEAN", "MINIMAL"],
   author: "resumerise",
   authorWebUrl: "http://resumerise.io",
-  subscription: "FREE"
 } as ResumeriseMeta;
--- render.ts
+++ render.ts
@@ -12,7 +12,11 @@ import {
   getWidgetSkillListFilePath,
 } from "./theme-library.ts";
 
-import { CompileException, getFileContent, Resume } from "./core-library.ts";
+import {
+  Resume,
+  CompileException,
+  getFileContent
+} from "./core-library.ts";
 
 export const render = async (
   resume: Resume,
@@ -23,8 +27,7 @@ export const render = async (
       "./templates/layout.eta",
       import.meta.url,
     );
-    const css = await getFileContent("./css/style.css", import.meta.url);
-
+    const mainCss = await getFileContent("./css/main.css", import.meta.url);
     const awardTemplateName = "awards";
     eta.templates.define(
       awardTemplateName,
@@ -168,11 +171,44 @@ export const render = async (
       ),
     );
 
+    const coverTemplate = "cover";
+    eta.templates.define(
+      coverTemplate,
+      eta.compile(
+        await getFileContent(
+          "./templates/cover.eta",
+          import.meta.url,
+        ),
+      ),
+    );
+
+    const tocTemplate = "toc";
+    eta.templates.define(
+      tocTemplate,
+      eta.compile(
+        await getFileContent(
+          "./templates/toc.eta",
+          import.meta.url,
+        ),
+      ),
+    );
+
     const dateRangeTemplateName = "date-range";
     eta.templates.define(
       dateRangeTemplateName,
       eta.compile(
         await getWidgetDateRangeFilePath(),
+      ),
+    );
+
+    const ratingTemplateName = "skill-rating";
+    eta.templates.define(
+      ratingTemplateName,
+      eta.compile(
+        await getFileContent(
+          "./templates/widgets/rating.eta",
+          import.meta.url,
+        ),
       ),
     );
 
@@ -213,6 +249,14 @@ export const render = async (
       listTemplateName,
       eta.compile(
         await getWidgetListFilePath(),
+      ),
+    );
+
+    const skillListTemplateName = "skillList";
+    eta.templates.define(
+      skillListTemplateName,
+      eta.compile(
+        await getWidgetSkillListFilePath(),
       ),
     );
 
@@ -279,7 +323,7 @@ export const render = async (
       orderedMap.set(resumeCategory, map.get(resumeCategory)!);
     });
     const result = await eta.render(layout, {
-      css: css,
+      mainCss: mainCss,
       widgetCss: widgetCss,
       resume: resume,
       type: type,
